8c30f86ecd608bc1dd263c8a365a51dff62c8443
alboom4ick <alibekomirzak400@gmail.com>
2025-06-24 21:38:04 +0500

    deleted the ondebug()

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -21,10 +21,6 @@ export const CLOUD_PROJECT_NUMBER = '109731201886';
 
 export const SITE_BASE = 'https://master.d3jesgto843lwi.amplifyapp.com';
 
-function inDebugMode() {
-  return process.env.NEXT_PUBLIC_DEBUG === '1';
-}
-
 /**
  * @see {@link https://developers.google.com/meet/add-ons/guides/overview#main-stage}
  */
